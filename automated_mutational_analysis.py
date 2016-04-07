977e7ba7ba94f08af90af60209f470234b886589
Morgan Nance <morganlnance@gmail.com>
2016-04-07 16:30:07 -0700

    automated_mutational_analysis now takes in and adds the utilities directory to the sys.path

--- automated_mutational_analysis.py
+++ automated_mutational_analysis.py
@@ -22,6 +22,7 @@ parser = argparse.ArgumentParser(description="Use PyRosetta to pack and minimize
 parser.add_argument("native_pdb_file", type=str, help="the filename of the native PDB structure")
 parser.add_argument("structure_directory", type=str, help="where do you want your decoys to be dumped? Each PDB will have its own directory there")
 parser.add_argument("mutant_list_file", help="give me the list of mutations you want to have made")
+parser.add_argument("utility_directory", type=str, help="where do the utility files live? Give me the directory.")
 parser.add_argument("--base_native_file", default=None, help="if you have it, give the the path to the low E base native structure you want to use. Default = None = make a low E base native structure.")
 parser.add_argument("--base_nstruct", type=int, default=1000, help="how many decoy structures do you want to create to get a base native structure? Default = 1000")
 parser.add_argument("--mut_nstruct", type=int, default=1000, help="how many decoy structures do you want to create to get a low E mutant structure? Default = 1000")
@@ -35,7 +36,6 @@ input_args = parser.parse_args()
 
 import os
 import sys
-sys.path.append( "/Users/Research/antibody_project/utility_functions" )
 
 # check to see that all of the files exist
 if not os.path.isfile( input_args.native_pdb_file ):
@@ -56,19 +56,24 @@ else:
     else:
         main_structure_dir = input_args.structure_directory + '/'
 
+# check the utility directory
+if not os.path.isdir( input_args.utility_directory ):
+    print "Your argument", input_args.utility_directory, "for utility_directory is not a directory, exiting"
+    sys.exit()
+
+# add the utility directory to the system path for loading of modules
+sys.path.append( input_args.utility_directory )
+
 # make the needed directories if needed
-# base_structs, lowest_E_structs, and mut_structs with
+# base_structs and lowest_E_structs
 # input_args.structure_directory as the base directory
 base_structs_dir = main_structure_dir + "base_structs/"
 lowest_E_structs_dir = main_structure_dir + "lowest_E_structs/"
-mut_structs_dir = main_structure_dir + "mut_structs/"
 
 if not os.path.isdir( base_structs_dir ):
     os.mkdir( base_structs_dir )
 if not os.path.isdir( lowest_E_structs_dir ):
     os.mkdir( lowest_E_structs_dir )
-if not os.path.isdir( mut_structs_dir ):
-    os.mkdir( mut_structs_dir )
 
 # relay information to user
 print
@@ -78,7 +83,6 @@ if input_args.base_native_file is not None:
 print "Main structure directory:\t", main_structure_dir
 print "Base structures directory:\t", base_structs_dir
 print "Lowest E structures directory:\t", lowest_E_structs_dir
-print "Mutant structures directory:\t", mut_structs_dir
 print
 
 

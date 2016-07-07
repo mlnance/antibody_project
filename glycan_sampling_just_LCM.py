cce082935a33ceff3508f8ac8c46acbd84e39d49
Morgan Nance <morganlnance@gmail.com>
2016-07-07 18:18:31 -0400

    forgot to erase my notes from chatting with Jason

--- glycan_sampling_just_LCM.py
+++ glycan_sampling_just_LCM.py
@@ -436,9 +436,6 @@ while not jd.job_complete:
 
     ## use the LinkageConformerMover find a local sugar minima
     # make a MoveMap for these Fc sugars
-    # chi = True, just do it and for Small
-    # bb = phi, psi, and omega
-    # 1 move = 1 move SmallMover
     lcm_mm = make_movemap_for_range( Fc_sugar_nums_except_core_GlcNAc, 
                                      allow_bb_movement = True, 
                                      allow_chi_movement = True )

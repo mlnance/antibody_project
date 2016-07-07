ff39e9f780a4cde62df133ddc8479814b664981e
Morgan Nance <morganlnance@gmail.com>
2016-07-07 12:39:41 -0400

    added more informative comments for the FoldTree and JUMP section of pose_metrics_util. Also uncommented the pmm parts

--- utility_functions/pose_metrics_util.py
+++ utility_functions/pose_metrics_util.py
@@ -23,9 +23,9 @@ def pseudo_interface_score_3ay4( pose, sf, native = False, pmm = None ):
     if not native:
         ft = FoldTree()
         ft.add_edge( 1, 215, -1 )
-        ft.add_edge( 1, 216, 1 )
+        ft.add_edge( 1, 216, 1 )    # beginning of chain A to beginning of chain B
         ft.add_edge( 216, 431, -1 )
-        ft.add_edge( 1, 432, 2 )
+        ft.add_edge( 1, 432, 2 )    # beginning of chain A to beginning of chain C
         ft.add_edge( 432, 591, -1 )
         ft.add_edge( 579, 592, "ND2", "C1" )
         ft.add_edge( 592, 596, -1 )
@@ -34,11 +34,11 @@ def pseudo_interface_score_3ay4( pose, sf, native = False, pmm = None ):
         ft.add_edge( 592, 599, "O6", "C1" )
         ft.add_edge( 462, 600, "ND2", "C1" )
         ft.add_edge( 600, 602, -1 )
-        ft.add_edge( 69, 603, 3 )
+        ft.add_edge( 69, 603, 3 )   # ASN 297 A to core GlcNAc H
         ft.add_edge( 603, 607, -1 )
         ft.add_edge( 605, 608, "O6", "C1" )
         ft.add_edge( 608, 610, -1 )
-        ft.add_edge( 284, 611, 4 )
+        ft.add_edge( 284, 611, 4 )  # ASN 297 B to core GlcNAc J
         ft.add_edge( 611, 615, -1 )
         ft.add_edge( 613, 616, "O6", "C1" )
         ft.add_edge( 616, 618, -1 )
@@ -47,17 +47,17 @@ def pseudo_interface_score_3ay4( pose, sf, native = False, pmm = None ):
     else:
         ft = FoldTree()
         ft.add_edge( 1, 215, -1 )
-        ft.add_edge( 69, 216, 1 )
+        ft.add_edge( 69, 216, 1 )   # ASN 297 A to core GlcNAc D
         ft.add_edge( 216, 220, -1 )
         ft.add_edge( 218, 221, "O6", "C1" )
         ft.add_edge( 221, 223, -1 )
-        ft.add_edge( 1, 224, 2 )
+        ft.add_edge( 1, 224, 2 )    # beginning of chain A to beginning of chain B
         ft.add_edge( 224, 439, -1 )
-        ft.add_edge( 292, 440, 3 )
+        ft.add_edge( 292, 440, 3 )  # ASN 297 B to core GlcNAc E
         ft.add_edge( 440, 444, -1 )
         ft.add_edge( 442, 445, "O6", "C1" )
         ft.add_edge( 445, 447, -1 )
-        ft.add_edge( 1, 448, 4 )
+        ft.add_edge( 1, 448, 4 )    # beginning of chain A to beginning of chain C
         ft.add_edge( 448, 607, -1 )
         ft.add_edge( 595, 608, "ND2", "C1" )
         ft.add_edge( 608, 612, -1 )
@@ -68,56 +68,56 @@ def pseudo_interface_score_3ay4( pose, sf, native = False, pmm = None ):
         ft.add_edge( 616, 618, -1 )
         
     # make a temporary Pose and give the new FoldTree to it
-    #try:
-    #    pmm.keep_history( True )
-    #except:
-    #    pass
+    try:
+        pmm.keep_history( True )
+    except:
+        pass
     temp_pose = Pose()
     temp_pose.assign( pose )
-    #try:
-    #    pmm.apply( temp_pose )
-    #except:
-    #    pass
     temp_pose.fold_tree( ft )
+    try:
+        pmm.apply( temp_pose )
+    except:
+        pass
 
     # split apart the two Fc sugars one-by-one
-    # if decoy structure
+    # if decoy structure -- the two glycans are now the last two new jumps
     if not native:
         jump = temp_pose.jump( 3 ) # sugar A
         vec = xyzVector_Real( 1000, 1000, 1000 )
         jump.set_translation( vec )
         temp_pose.set_jump( 3, jump )
-        #try:
-        #    pmm.apply( temp_pose )
-        #except:
-        #    pass
+        try:
+            pmm.apply( temp_pose )
+        except:
+            pass
         jump = temp_pose.jump( 4 ) # sugar B
         vec = xyzVector_Real( 1000, 1000, 1000 )
         jump.set_translation( vec )
         temp_pose.set_jump( 4, jump )
-        #try:
-        #    pmm.apply( temp_pose )
-        #except:
-        #    pass
+        try:
+            pmm.apply( temp_pose )
+        except:
+            pass
 
-    # else native structure
+    # else native structure -- the two glycans are the first and third new jumps
     else:
         jump = temp_pose.jump( 1 ) # sugar A
         vec = xyzVector_Real( 1000, 1000, 1000 )
         jump.set_translation( vec )
         temp_pose.set_jump( 1, jump )
-        #try:
-        #    pmm.apply( temp_pose )
-        #except:
-        #    pass
+        try:
+            pmm.apply( temp_pose )
+        except:
+            pass
         jump = temp_pose.jump( 3 ) # sugar B
         vec = xyzVector_Real( 1000, 1000, 1000 )
         jump.set_translation( vec )
         temp_pose.set_jump( 3, jump )
-        #try:
-        #    pmm.apply( temp_pose )
-        #except:
-        #    pass        
+        try:
+            pmm.apply( temp_pose )
+        except:
+            pass        
 
     # score the split-apart Pose
     split_score = sf( temp_pose )

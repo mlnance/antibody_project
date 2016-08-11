fb6d9236a7fb6ebdc2775fe878dde6afc8274247
Morgan Nance <morganlnance@gmail.com>
2016-08-11 18:01:54 -0400

    huge revamp to alanine_scan. The old functions didn't work anymore anyway

--- utility_functions/alanine_scan.py
+++ utility_functions/alanine_scan.py
@@ -1,119 +1,148 @@
-# import rosetta-specific modules
-from rosetta import init, pose_from_pdb, get_fa_scorefxn, \
-    standard_packer_task, change_cys_state, \
-    Pose, MoveMap, RotamerTrialsMover, MinMover, \
-    PyMOL_Mover
-from toolbox import mutate_residue
+#!/usr/bin/python
+__author__="morgalnance"
 
-# imports for data analysis
-import os.path
-import sys
+
+# import Rosetta functions
+from antibody_functions import initialize_rosetta, load_pose, \
+    make_pack_rotamers_mover, get_res_nums_within_radius
+from rosetta import Pose, get_fa_scorefxn, PyMOL_Mover, \
+    MoveMap, MinMover
+from rosetta.core.chemical import VariantType
+from rosetta.core.pose import remove_variant_type_from_pose_residue
+
+# mutation-related imports
+from rosetta import pose_from_sequence, ResidueFactory
+#from rosetta.protocols.simple_moves import MutateResidue
+#from toolbox import mutate_residue
+
+# general imports
+import os, sys
 import pandas as pd
+
+
 
 # create global pymol object for viewing
 pmm = PyMOL_Mover()
+pmm.keep_history()
 
 # path for mutational analysis data
-data_dir = "/Users/Research/pyrosetta_repo/mutational_data/"
+data_dir = os.getcwd() + "/mutational_data/"
+if not os.path.isdir( data_dir ):
+    os.mkdir( data_dir )
 data_filename = "alanine_scan.csv"
 
 # initialize rosetta with sugar flags
-init(extra_options="-include_sugars -override_rsd_type_limit -read_pdb_link_records")
+initialize_rosetta()
 
 # create pose object, assign info object, and send pose to pymol
-pose = pose_from_pdb("Fc_FcgRIII.pdb")
-info = pose.pdb_info()
-#pmm.apply(pose)
+native_pose = load_pose( "/Users/Research/pyrosetta_dir/structures_from_jazz/3ay4_Fc_FcgRIIIa/fresh_start/project_structs/lowest_E_double_pack_and_min_only_native_crystal_struct_3ay4_Fc_FcgRIII.pdb" )
+native_pose.pdb_info().name( "native_pose" )
+info = native_pose.pdb_info()
+pmm.apply( native_pose )
 
 # instantiate full atom score function and score pose
 sf = get_fa_scorefxn()
-start_score = sf(pose)
-
-# create packer task
-task = standard_packer_task(pose)
-task.restrict_to_repacking()
-task.or_include_current(True)
-
-# run a RotamerTrialsMover
-pack_rotamers_mover = RotamerTrialsMover(sf, task)
-pack_rotamers_mover.apply(pose)
-#pmm.apply(pose)
-
-# insantiate a movemap
-mm = MoveMap() 
-mm.set_bb(True)
-mm.set_chi(True)
-mm.set_nu(True)
-
-# turn off chi and nu for sugar residues
-## this has been hardcoded!
-sugar_res_pos = []
-for x in range(216,223 + 1) + range(440,447 + 1) + range(608,618 + 1):
-    sugar_res_pos.append(x)
-for x in sugar_res_pos:
-    mm.set_chi(x, False)
-    mm.set_nu(x, False)
-    
-# minimize using MinMover
-min_mover = MinMover(mm, sf, "dfpmin", 0.01, True)
-min_mover.apply(pose)
-
-# grab post-minimization score and send to pymol
-post_min_score = sf(pose)
-#pmm.apply(pose)
+native_E_start = sf( native_pose )
 
 
+# create some constant data
+AA = 'ALA'
+PACK_RADIUS = 5.0
+ALA_pose = pose_from_sequence( "AAA" )
 
-# check to see if alanine scan has already been done
-# if yes, print data
-if(os.path.exists(data_dir + data_filename)):
-    print "\nAlanine scan has already been ran! Check mutational_data for csv file."
-    df = pd.DataFrame.from_csv(data_dir + data_filename)
-    print df
-    sys.exit()
-    
-# if no, get data
-else:
-    # create some constant data
-    AA = 'A'
-    PACK_RADIUS = 10.0
-    
-    # insantiate lists for data (will be put into pandas dataframe)
-    orig_AA = []
-    position = []
-    native_E = []
-    mut_E = []
-    ddG = []
-    
-    # for all residues, mutate to alanine
-    for seq_pos in range(1, pose.total_residue() + 1):
-        res = pose.residue(seq_pos)
-        
-        # make sure residue is not a disulfide, sugar, or a branch point
-        if res.name() != "CYD":
-            if not res.is_carbohydrate():
-                if res.name1() != 'A':
-                    if not res.is_branch_point():
-                        # append original residue, position, and score to list
-                        orig_AA.append(pose.residue(seq_pos).name1())
-                        position.append(seq_pos)
-                        native_E.append(sf(pose))
-                        
-                        # mutate!
-                        print "Mutating position", seq_pos, "..."
-                        mutant = mutate_residue(pose, seq_pos, AA, PACK_RADIUS, sf)
-                    	#pmm.apply(mutant)
-                        
-                        # minimize mutation
-                        min_mover.apply(mutant)
-                    	#pmm.apply(mutant)
-                        
-                        # score and add to list for dataframe
-                        new_E = sf(mutant)
-                        mut_E.append(new_E)
-                        ddG.append(new_E - sf(pose))
-                    
-                
+# insantiate lists for data (will be put into pandas dataframe)
+orig_AA = []
+position = []
+native_E = []
+mut_E = []
+ddG = []
+
+# for all non-sugar residues, mutate to alanine
+#    for seq_pos in range(1, native_pose.total_residue() + 1):
+for seq_pos in range(8, 9):
+    res = native_pose.residue( seq_pos )
+
+    # make sure residue is not a sugar or a branch point
+    # also skipping Cys residues because they're just being sassy
+    # replacing alanines too for the heck of it. The ddG should be very close to 0 if this works
+    if not res.name1() == 'C':
+        if not res.is_carbohydrate():
+            if not res.is_branch_point():
+                # append original residue, position, and score to list
+                orig_AA_name1 = str( native_pose.residue( seq_pos ).name1() )
+                orig_AA.append( orig_AA_name1 )
+                position.append( seq_pos )
+                native_E.append( native_E_start )
+
+                # get a copy of the Pose
+                mutant = Pose()
+                mutant.assign( native_pose )
+
+                # build the new Ala residue based on the position of the old residue
+                # if this is the first residue of the pose
+                if seq_pos == 1:
+                    # this is the N-terminal end, so use an N-terminal ALA residue
+                    ALA_restype = ALA_pose.conformation().residue_type( 1 )
+
+                # if this is the last residue of the pose
+                elif seq_pos == native_pose.n_residue():
+                    # this is the C-terminal end, so use an C-terminal ALA residue
+                    ALA_restype = ALA_pose.conformation().residue_type( 3 )
+
+                # otherwise, this is a residue with something bonded on both sides
+                else:
+                    ALA_restype = ALA_pose.conformation().residue_type( 2 )
+
+                # build the Ala
+                res_factory = ResidueFactory()
+
+                # this uses the backbone information from the current residue at this position to build the new Ala
+                # if this isn't a Gly residue, use the CB information as well
+                if mutant.residue( seq_pos ).name1() != "G":
+                    new_ALA_res = res_factory.create_residue( ALA_restype, mutant.residue( seq_pos ), mutant.conformation(), preserve_c_beta=True )
+                # otherwise, don't preserve CB because Gly doesn't have a CB
+                else:
+                    new_ALA_res = res_factory.create_residue( ALA_restype, mutant.residue( seq_pos ), mutant.conformation() )
+
+
+                # mutate!
+                mutant.replace_residue( seq_pos, new_ALA_res, orient_backbone=True )
+
+                # change pose name to (orig AA)(pdb number)_to_A
+                mutant.pdb_info().name( "%s%s_to_A_withCB" %( orig_AA_name1, str( mutant.pdb_info().pose2pdb( seq_pos ).strip().replace( ' ', '' ) ) ) )
+
+
+                # get residue numbers (including mutation site) to be packed and minimized
+                res_nums_around_mutation = get_res_nums_within_radius( seq_pos, mutant, PACK_RADIUS, include_seq_pos = True )
+
+                # pack around mutation
+                pack_rotamers_mover = make_pack_rotamers_mover( sf, mutant,
+                                                                apply_sf_sugar_constraints = False,
+                                                                pack_branch_points = True,
+                                                                residue_range = res_nums_around_mutation )
+                pack_rotamers_mover.apply( mutant )
+
+                # minimize around mutation
+                min_mm = MoveMap()
+                for res_num in res_nums_around_mutation:
+                    min_mm.set_bb( res_num, True )
+                    min_mm.set_chi( res_num, True )
+                min_mover = MinMover( movemap_in = min_mm,
+                                      scorefxn_in = sf,
+                                      min_type_in = "dfpmin_strong_wolfe",
+                                      tolerance_in = 0.01,
+                                      use_nb_list_in = True )
+                min_mover.apply(mutant)
+
+                # visualize mutation
+                pmm.apply(mutant)
+
+                # score and add to list for dataframe
+                new_E = sf( mutant )
+                mut_E.append( new_E )
+                ddG.append( new_E - native_E_start )
+
+
 # print data and add data to dataframe
 df = pd.DataFrame()
 df["orig_AA"] = orig_AA

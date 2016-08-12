5f1578becdebe2b1d6c63392e2564ad9a5774609
Morgan Nance <morganlnance@gmail.com>
2016-08-12 15:01:48 -0400

    updating alanine_scan with more logic and a couple more metrics

--- utility_functions/alanine_scan.py
+++ utility_functions/alanine_scan.py
@@ -4,7 +4,9 @@ __author__="morgalnance"
 
 # import Rosetta functions
 from antibody_functions import initialize_rosetta, load_pose, \
-    make_pack_rotamers_mover, get_res_nums_within_radius
+    make_pack_rotamers_mover, get_res_nums_within_radius, \
+    native_Fc_glycan_nums, native_Fc_protein_nums, \
+    get_contact_map_between_range1_range2
 from rosetta import Pose, get_fa_scorefxn, PyMOL_Mover, \
     MoveMap, MinMover
 from rosetta.core.chemical import VariantType
@@ -38,7 +40,7 @@ initialize_rosetta()
 native_pose = load_pose( "/Users/Research/pyrosetta_dir/structures_from_jazz/3ay4_Fc_FcgRIIIa/fresh_start/project_structs/lowest_E_double_pack_and_min_only_native_crystal_struct_3ay4_Fc_FcgRIII.pdb" )
 native_pose.pdb_info().name( "native_pose" )
 info = native_pose.pdb_info()
-pmm.apply( native_pose )
+#pmm.apply( native_pose )
 
 # instantiate full atom score function and score pose
 sf = get_fa_scorefxn()
@@ -52,7 +54,10 @@ ALA_pose = pose_from_sequence( "AAA" )
 
 # insantiate lists for data (will be put into pandas dataframe)
 orig_AA = []
-position = []
+pose_position = []
+pdb_position = []
+pdb_chain = []
+within_10A_of_Fc_glycan_res = []
 native_E = []
 native_E_res = []
 mut_E = []
@@ -63,10 +68,17 @@ ddG_res = []
 # used to build a new Ala residue
 res_factory = ResidueFactory()
 
+# get the 10A contact map between Fc protein and Fc glycan for use in determining if the Fc protein mutation
+# is within 10A of an Fc glycan residue
+Fc_protein_to_Fc_glycan_cmap = get_contact_map_between_range1_range2( native_Fc_protein_nums, native_Fc_glycan_nums, native_pose, cutoff = 10 )
+
+
 # mutate all residues to Ala
 # make sure residue is not a sugar or a branch point
 # also skipping Cys residues because they're just being sassy
-for seq_pos in range(1, native_pose.total_residue() + 1):
+residues_that_didnt_work = []
+#for seq_pos in range(1, native_pose.total_residue() + 1):
+for seq_pos in native_Fc_protein_nums:
     res = native_pose.residue( seq_pos )
 
     # replacing alanines too for the heck of it. The ddG should be very close to 0 if this works
@@ -76,9 +88,19 @@ for seq_pos in range(1, native_pose.total_residue() + 1):
                 # append original residue, position, and score to list
                 orig_AA_name1 = str( native_pose.residue( seq_pos ).name1() )
                 orig_AA.append( orig_AA_name1 )
-                position.append( seq_pos )
+                pose_position.append( seq_pos )
+                pdb_position.append( native_pose.pdb_info().pose2pdb( seq_pos ).strip().split( ' ' )[0] )
+                pdb_chain.append( native_pose.pdb_info().pose2pdb( seq_pos ).strip().split( ' ' )[1] )
                 native_E.append( nat_E )
 
+                # if this is an Fc protein residue, determine if it is within 10A of the Fc glycan
+                # keeping this here for now in case I decide to do the alanine scan on the whole protein
+                if seq_pos in native_Fc_protein_nums:
+                    # use the contact map created earlier to determine if this residue is near the glycan
+                    # since the contact map was made between the protein and glycan, just check to see if this res is a key
+                    within_10A_of_Fc_glycan_res.append( seq_pos in Fc_protein_to_Fc_glycan_cmap.keys() )
+                        
+                        
                 # get a copy of the Pose
                 mutant = Pose()
                 mutant.assign( native_pose )
@@ -109,57 +131,65 @@ for seq_pos in range(1, native_pose.total_residue() + 1):
 
 
                 # mutate!
-                mutant.replace_residue( seq_pos, new_ALA_res, orient_backbone=True )
+                try:
+                    mutant.replace_residue( seq_pos, new_ALA_res, orient_backbone=True )
 
-                # change pose name to (orig AA)(pdb number)_to_A
-                mutant.pdb_info().name( "%s%s_to_A_withCB" %( orig_AA_name1, str( mutant.pdb_info().pose2pdb( seq_pos ).strip().replace( ' ', '' ) ) ) )
-
-
-                # get residue numbers (including mutation site) to be packed and minimized
-                res_nums_around_mutation = get_res_nums_within_radius( seq_pos, mutant, PACK_RADIUS, include_seq_pos = True )
-
-                # pack around mutation
-                pack_rotamers_mover = make_pack_rotamers_mover( sf, mutant,
-                                                                apply_sf_sugar_constraints = False,
-                                                                pack_branch_points = True,
-                                                                residue_range = res_nums_around_mutation )
-                pack_rotamers_mover.apply( mutant )
-
-                # minimize around mutation
-                min_mm = MoveMap()
-                for res_num in res_nums_around_mutation:
-                    min_mm.set_bb( res_num, True )
-                    min_mm.set_chi( res_num, True )
-                min_mover = MinMover( movemap_in = min_mm,
-                                      scorefxn_in = sf,
-                                      min_type_in = "dfpmin_strong_wolfe",
-                                      tolerance_in = 0.01,
-                                      use_nb_list_in = True )
-                min_mover.apply(mutant)
-
-                # visualize mutation
-                pmm.apply(mutant)
+                    # change pose name to (orig AA)(pdb number)_to_A
+                    mutant.pdb_info().name( "%s%s_to_A" %( orig_AA_name1, str( mutant.pdb_info().pose2pdb( seq_pos ).strip().replace( ' ', '' ) ) ) )
 
 
-                # score and add to list for dataframe
-                new_E = sf( mutant )
-                new_E_res = mutant.energies().residue_total_energy( seq_pos )
-                nat_E_res = native_pose.energies().residue_total_energy( seq_pos )
+                    # get residue numbers (including mutation site) to be packed and minimized
+                    res_nums_around_mutation_site = get_res_nums_within_radius( seq_pos, mutant, PACK_RADIUS, include_seq_pos = True )
 
-                mut_E.append( new_E )
-                mut_E_res.append( new_E_res )
-                native_E_res.append( nat_E_res )
-                ddG.append( new_E - nat_E )
-                ddG_res.append( new_E_res - nat_E_res )
+
+                    # pack around mutation
+                    pack_rotamers_mover = make_pack_rotamers_mover( sf, mutant,
+                                                                    apply_sf_sugar_constraints = False,
+                                                                    pack_branch_points = True,
+                                                                    residue_range = res_nums_around_mutation_site )
+                    pack_rotamers_mover.apply( mutant )
+
+                    # minimize around mutation
+                    min_mm = MoveMap()
+                    for res_num in res_nums_around_mutation_site:
+                        min_mm.set_bb( res_num, True )
+                        min_mm.set_chi( res_num, True )
+                    min_mover = MinMover( movemap_in = min_mm,
+                                          scorefxn_in = sf,
+                                          min_type_in = "dfpmin_strong_wolfe",
+                                          tolerance_in = 0.01,
+                                          use_nb_list_in = True )
+                    min_mover.apply(mutant)
+
+                    # visualize mutation
+                    #pmm.apply(mutant)
+
+
+                    # score and add to list for dataframe
+                    new_E = sf( mutant )
+                    new_E_res = mutant.energies().residue_total_energy( seq_pos )
+                    nat_E_res = native_pose.energies().residue_total_energy( seq_pos )
+
+                    mut_E.append( new_E )
+                    mut_E_res.append( new_E_res )
+                    native_E_res.append( nat_E_res )
+                    ddG.append( new_E - nat_E )
+                    ddG_res.append( new_E_res - nat_E_res )
+                except:
+                    residues_that_didnt_work.append( seq_pos )
+                    pass
 
 
 # print data and add data to dataframe
 df = pd.DataFrame()
 df["orig_AA"] = orig_AA
-df["seq_pos"] = position
+df["pose_num"] = pose_position
+df["pdb_num"] = pdb_position
+df["pdb_chain"] = pdb_chain
+df["within_10A_of_Fc_glycan_res"] = within_10A_of_Fc_glycan_res
 df["native_E"] = native_E
-df["native_E_res"] = native_E_res
 df["mut_E"] = mut_E
+df["native_E_res"] = native_E_res
 df["mut_E_res"] = mut_E_res
 df["ddG"] = ddG
 df["ddG_res"] = ddG_res

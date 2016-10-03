c78b02266ac49a1d49735bf050c4ea50898fac8d
mlnance <morganlnance@gmail.com>
2016-10-03 11:37:24 -0400

    protocol 4 will test an LCM reset without population ideals. Still trying 100 SSM, will try 200 SSM if the funnel is no good

--- native_3ay4_glycan_modeling.py
+++ native_3ay4_glycan_modeling.py
@@ -242,6 +242,41 @@ elif input_args.protocol_num == 3:
     # write information to file (also prints to screen)
     GlycanModelProtocol.write_protocol_info_file( native_pose, input_args.protocol_num )
 
+elif input_args.protocol_num == 4:
+    # create the necessary minimization (and overall movement) MoveMap for Protocol_4 version
+    mm = MoveMap()
+    for res_num in native_Fc_glycan_nums_except_core_GlcNAc:
+        mm.set_bb( res_num, True )
+        mm.set_chi( res_num, False )
+        if native_pose.residue( res_num ).is_branch_point():
+            mm.set_branches( res_num, False )
+
+    # create the desired scorefxn
+    sf = get_fa_scorefxn_with_given_weights( { "fa_intra_rep" : 0.44 } )
+
+    # Protocol_4 creation and argument setting
+    GlycanModelProtocol = Model3ay4Glycan( mm_in = mm, 
+                                           sf_in = sf, 
+                                           angle_max = 6.0 * 3,  # 6.0 comes from default angle_max from SmallMover and ShearMover
+                                           dump_dir = input_args.structure_dir, 
+                                           pmm = pmm )
+    GlycanModelProtocol.trials = 100
+    GlycanModelProtocol.moves_per_trial = 3
+    GlycanModelProtocol.LCM_reset = True
+    GlycanModelProtocol.use_population_ideal_LCM_reset = False
+    GlycanModelProtocol.set_native_omega = False
+    GlycanModelProtocol.ramp_sf = True
+    GlycanModelProtocol.fa_atr_ramp_factor = 2.0
+    GlycanModelProtocol.fa_rep_ramp_factor = 0.5
+    GlycanModelProtocol.minimize_each_round = True
+    GlycanModelProtocol.make_small_moves = True
+    GlycanModelProtocol.make_shear_moves = False
+    GlycanModelProtocol.constraint_file = None
+    GlycanModelProtocol.verbose = True
+
+    # write information to file (also prints to screen)
+    GlycanModelProtocol.write_protocol_info_file( native_pose, input_args.protocol_num )
+
 # else I haven't made this protocol number yet
 else:
     print "\nI haven't created the protocol number you gave me yet.\n"
